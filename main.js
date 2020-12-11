b378a1ece087ec60ff3f1b1c10abd3165ba3b4cc
ZOmohundro <zaneomohundro@gmail.com>
2020-12-10 20:02:38 -0500

    create initial operations

--- main.js
+++ main.js
@@ -16,3 +16,36 @@ function refreshDisplayField (){
 }
 
 refreshDisplayField();
+
+
+//arithmetic functions
+add = ((a,b) => a+b);
+subtract = ((a,b) => a-b);
+multiply = ((a,b) => a*b);
+function divide (a,b) {
+    if (b != 0){
+        return a/b;
+    } else {
+        return "Divide by 0";
+    }
+};
+
+function operate (a,b,operator){
+    switch(operator) {
+        case "plus":
+            answer = add(a,b);
+            break;
+        case "subtract":
+            answer = subtract(a,b);
+            break;
+        case "multiply":
+            answer = multiply(a,b);
+            break;
+        case "divide":
+            answer = divide(a,b);
+            break;        
+    }
+    return answer;
+}
+
+console.log(operate(4,2,"divide"));
